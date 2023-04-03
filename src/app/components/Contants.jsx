13a200777dae9c58ba8017f3588505196973ac2e
Shrestho <shaishabdassrasto@gmail.com>
2023-04-03 09:53:33 +0600

    server rootURL

--- src/app/components/Contants.jsx
+++ src/app/components/Contants.jsx
@@ -1,3 +1,3 @@
 let RootURL;
-// export default RootURL = 'https://api.anytimecleaning.net/public/api/';
-export default RootURL = 'http://localhost:8000/api/';
+export default RootURL = 'https://api.anytimecleaning.net/public/api/';
+// export default RootURL = 'http://localhost:8000/api/';
