ff1146dfe63de3e76dde8c63aaf5fa8836c5b6e0
Hemel Hasan <38416727+MHemelHasan@users.noreply.github.com>
2023-05-25 16:19:55 +0600

    Update Contants.jsx

--- src/app/components/Contants.jsx
+++ src/app/components/Contants.jsx
@@ -1,4 +1,4 @@
 let RootURL;
-export default RootURL = 'https://anytimecleaning.net/api/public/api/';
+export default RootURL = 'https://api.anytimecleaning.net/public/api/';
 // eslint-disable-next-line no-unused-vars
 // export default RootURL = 'http://localhost:8000/api/';
